64c4a42395e48a6245451d8e62a6eae8b5182fcb
Stephen A Brooks <krammer439298@gmail.com>
2021-08-23 10:24:24 -0700

    changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Catalog()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
